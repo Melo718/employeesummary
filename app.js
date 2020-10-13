34e78db7a7179e3897e53ba5c71bef4cbbdd98f0
camilo alvarez <camiloalvarez88@gmail.com>
2020-10-12 23:51:31 -0400

    my second commit

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-
+const Manager = require("./lib");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-
 const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
